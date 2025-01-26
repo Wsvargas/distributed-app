2efc163a68397af9b5bf0e12424f43121624b96a
AlexandraN2001 <nacabrera@uce.edu.ec>
2025-01-25 22:47:29 -0500

    Uploading crud user

--- services/history/go.mod
+++
@@ -1,3 +0,0 @@
-go mod init history
-go get github.com/gorilla/mux
-go get github.com/lib/pq
--- services/reservations/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///reservations.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False

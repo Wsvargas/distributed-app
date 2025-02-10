e27c45027a9e0b2ac414b646790b709f152fa60c
AlexandraN2001 <nacabrera@uce.edu.ec>
2025-02-10 18:31:20 -0500

    edit dockercompose

--- backend/Flight-Management/update_flight/config.py
+++ backend/Flight-Management/update_flight/config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+     SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgresql://postgres:D1str1bu1D4.@flight-management-db.c7u4ume2skf9.us-east-1.rds.amazonaws.com:5432/flight_management'
+    )
+     SQLALCHEMY_TRACK_MODIFICATIONS = False

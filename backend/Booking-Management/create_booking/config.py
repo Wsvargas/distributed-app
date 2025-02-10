8fb98fb466b62a996ac6b4e8bfcbccfe8a5d386e
AlexandraN2001 <nacabrera@uce.edu.ec>
2025-02-10 18:45:51 -0500

    feat: flight

--- backend/Booking-Management/create_booking/config.py
+++ backend/Booking-Management/create_booking/config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgresql://postgres:D1str1bu1D4.@booking-management-db.c7u4ume2skf9.us-east-1.rds.amazonaws.com:5432/booking_management'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
